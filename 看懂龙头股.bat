cec80ab410e2dd326d32bbc9781fc7afde3e8ca3
pitucre <47495912+pitucre@users.noreply.github.com>
2020-06-28 11:37:49 +0800

    Add files via upload
    批处理文件

---
+++ 看懂龙头股.bat
@@ -0,0 +1,6 @@
+#python  F:\File\stock\爱在冰川\gzh_download_html_爱在冰川.py
+python  F:\File\stock\看懂龙头股\gzh_download_html_看懂龙头股.py
+#python  F:\File\stock\盘口逻辑拆解\gzh_download_html_盘口逻辑拆解.py
+#python  F:\File\stock\下注的快感\gzh_download_html_下注的快感.py
+#python  F:\File\stock\陶博士2006\gzh_download_html_陶博士2006.py
+pause
